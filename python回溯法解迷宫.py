060fc1047f3e48150160dac8a0f6c3a7eaed2a72
Tidus1991 <j554158979@gmail.com>
2017-10-20 12:18:07 +0800

    Add files via upload

---
+++ python回溯法解迷宫.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 12:17:02 2017
+
+@author: Tidus
+"""
+
+
+class SStack():
+    def __init__(self):
+        self.elems = []
+    
+    def is_empty(self):
+        return self.elems == []
+    
+    def top(self):
+        if self.is_empty():
+            return
+        return self.elems[-1]
+    
+    def push(self, elem):
+        self.elems.append(elem)
+        
+    def pop(self):
+        if self.is_empty():
+            return 
+        return self.elems.pop()
+    
+    def depth(self):
+        return len(self.elems)
+
+def mark(maze, pos):
+    maze[pos[0]][pos[0]] = 2
+    
+def passable(maze, pos):
+    return maze[pos[0]][pos[1]] == 0
+
+def print_path(end, pos, st):
+    path = []
+    path.append(st.pop())
+    path.reverse()
+    print('路径为：',path)
+    print('终点为：',end)
+    print('当前位置：',pos)
+    
+dirs = [(0,1), (1,0), (0,-1), (-1,0)]
+def find_path(maze, start ,end):
+    if start == end :
+        print(start)
+        return
+    st = SStack()
+    mark(maze, start)
+    st.push(start, 0)
+    while not st.is_empty():
+        pos, nxt = st.pop()
+        for i in range(nxt, 4):
+            nextp = (pos[0] + dirs[i][0],
+                     pos[1] + dirs[i][1])
+            if nextp == end:
+                print_path(end, pos ,st)
+                return
+            if passable(maze, nextp):
+                st.push(pos, i+1)
+                mark(maze, nextp)
+                st.push((nextp, 0))
+                break
+    print('No path found!')
