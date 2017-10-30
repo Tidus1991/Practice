3bc839d5803f1a7b75816b3f0d9e26cf0bab2cd4
Tidus1991 <j554158979@gmail.com>
2017-10-30 13:03:00 +0800

    Update python快速排序.py

--- python快速排序.py
+++ python快速排序.py
@@ -4,12 +4,6 @@ Created on Tue Sep 12 10:54:01 2017
 
 @author: Tidus
 """
-#import re
-arr = input()
-#lists = re.findall(r'.{1}',lists)
-arr = arr.split(' ')
-arr = list(map(int, arr))
-
 def QuickSort(arr):
     less = []
     pivotList = []
@@ -17,23 +11,16 @@ def QuickSort(arr):
     if len(arr) <= 1:
         return arr
     else:
-        pivot = arr[0]      #将第一个值做为基准
+        pivot = arr[0]      
         for i in arr:
             if i < pivot:
-                less.append(i)
                 print('less:',less)
             elif i > pivot:
                 more.append(i)
-                print('more:',more)
             else:
                 pivotList.append(i)
-                print('pivotList:',pivotList)
-
-        less = QuickSort(less)      #得到第一轮分组之后，继续将分组进行下去。Key Step!
+        less = QuickSort(less)     
         more = QuickSort(more)
-        print('a:',less)
-        print('b:',pivotList)
-        print('c:',more)
         return less + pivotList + more
     
 
